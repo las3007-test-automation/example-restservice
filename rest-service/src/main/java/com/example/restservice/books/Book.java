f1e46396efdf40d486cc21122378a64bfc3f917f
keithspiteri91 <45069595+keithspiteri91@users.noreply.github.com>
2024-06-18 16:16:14 +0200

    Removing Rest Service Project
    Removing Rest Service Project

--- rest-service/src/main/java/com/example/restservice/books/Book.java
+++
@@ -1,6 +0,0 @@
-package com.example.restservice.books;
-
-import jakarta.validation.constraints.NotEmpty;
-
-public record Book(Long id, @NotEmpty(message = "Book title cannot be null") String title, String author) {
-}
